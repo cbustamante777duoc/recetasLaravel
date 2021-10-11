90b74bf737e2420cfeb1988a192e253a5f093a11
cristian <cra.bustamante@alumnos.duoc.cl>
2021-10-11 19:07:29 -0300

    mejorando aparencia  recetas index

--- resources/views/recetas/index.blade.php
+++ resources/views/recetas/index.blade.php
@@ -1,11 +1,18 @@
-<h1>Pagina de recetas</h1>
+@extends('layouts.app')
 
-@foreach ($recetas as $receta )
-    <li>{{$receta}}</li>
-@endforeach
-<hr>
 
-<h2>Categorias</h2>
-@foreach ($categorias as $categoria )
-    <li>{{$categoria}}</li>
-@endforeach
+@section('content')
+
+    <h1>Pagina de recetas</h1>
+
+    @foreach ($recetas as $receta )
+        <li>{{$receta}}</li>
+    @endforeach
+    <hr>
+
+    <h2>Categorias</h2>
+    @foreach ($categorias as $categoria )
+        <li>{{$categoria}}</li>
+    @endforeach
+
+@endsection
