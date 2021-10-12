6758d000636805a988ffe7065ba0ad82a14a3820
cristian <cra.bustamante@alumnos.duoc.cl>
2021-10-12 18:35:25 -0300

    creando un seeder de categoria

---
+++ database/seeds/CategoriasSeeder.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Comida mexicana',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Comida chilena',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Comida tailandesa',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Comida japonesa',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Ensaladas',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Poster',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'otros',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Bebidas',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+
+    }
+}
