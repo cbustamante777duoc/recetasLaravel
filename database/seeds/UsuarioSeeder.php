b3ebd5eb531c3acedfac8a5347bba7f5d653895b
cristian <cra.bustamante@alumnos.duoc.cl>
2021-10-12 18:48:30 -0300

    creando un seed de user

--- database/seeds/UsuarioSeeder.php
+++ database/seeds/UsuarioSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsuarioSeeder extends Seeder
 {
@@ -11,6 +13,43 @@ class UsuarioSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Lisa',
+            'email' => 'correo@correo.com',
+            'password' => Hash::make('abcd1234'),
+            'url' => 'https://www.instagram.com',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Jennie',
+            'email' => 'correo2@correo.com',
+            'password' => Hash::make('abcd1234'),
+            'url' => 'https://www.instagram.com',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Rose',
+            'email' => 'correo3@correo.com',
+            'password' => Hash::make('abcd1234'),
+            'url' => 'https://www.instagram.com',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Jisoo',
+            'email' => 'correo4@correo.com',
+            'password' => Hash::make('abcd1234'),
+            'url' => 'https://www.instagram.com',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+
+
     }
 }
