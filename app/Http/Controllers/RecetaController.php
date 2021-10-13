f32de2bf7db6c4f379527573daecc45ea2e1355e
cristian <cra.bustamante@alumnos.duoc.cl>
2021-10-12 21:38:45 -0300

    agregando un achicador de imagenes

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -6,6 +6,7 @@ use App\Receta;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Intervention\Image\Facades\Image;
 
 class RecetaController extends Controller
 {
@@ -49,24 +50,38 @@ class RecetaController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request['imagen']->store('upload-recetas','public'));
+        // dd($request['imagen']->store('upload-recetas','public'));
 
-        //recibe todo todo lo que viene en el formulario
+        //valida los campos
         $data = request()->validate([
             'titulo' => 'required|min:6',
             'preparacion' => 'required',
             'ingredientes' => 'required',
             'categoria' => 'required',
             // 'imagen' => 'required|image|size:1000'
+            'imagen' => 'required|image'
         ]);
 
+        //guardando la ruta de ka imagen
+        $ruta_imagen = $request['imagen']->store('upload-recetas','public');
+
+        //guardando la imagen mas chica
+        $img = Image::make( public_path("storage/{$ruta_imagen}"))->fit(1000, 550);
+        $img->save();
+
+
+
+
+
+
+        //almacenar en la base de datos
         DB::table('recetas')->insert([
             'titulo' => $data['titulo'],
             'preparacion' => $data['preparacion'],
             'ingredientes' => $data['ingredientes'],
             'ingredientes' => $data['ingredientes'],
             'user_id' => Auth::user()->id,
-            'imagen' => 'imagen.jpg',
+            'imagen' => $ruta_imagen,
             'categoria_id' => $data['categoria'],
         ]);
 
